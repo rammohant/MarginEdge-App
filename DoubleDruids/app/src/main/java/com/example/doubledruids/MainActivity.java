9593216e78cde8edb202308bb592499c8155f3e6
Meredith Seaberg <meredithseabergaccount@Merediths-MacBook-Pro.local>
2020-09-26 18:19:11 -0400

    Image upload feature

--- DoubleDruids/app/src/main/java/com/example/doubledruids/MainActivity.java
+++ DoubleDruids/app/src/main/java/com/example/doubledruids/MainActivity.java
@@ -1,14 +1,150 @@
 package com.example.doubledruids;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+//import org.apache.commons.io.IOUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView img;
+    private File imageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        img = (ImageView) findViewById(R.id.imageView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_PICK = 2;
+
+    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+        Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE);
+
+        // from library
+//        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+//                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        startActivityForResult(pickPhoto, REQUEST_IMAGE_PICK);
+    }
+
+    /**
+     * Retrieve image picked by user and add to imageView
+     */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("msg", "I am here!");
+        Bitmap photo = (Bitmap) data.getExtras().get("data");
+        img.setImageBitmap(photo);
+        super.onActivityResult(requestCode, resultCode, data);
+        
+//        Log.i("msg", "I am here1");
+//        if (resultCode != RESULT_CANCELED) {
+//            Log.i("msg", "I am here2");
+//            switch (requestCode) {
+//                case 1:
+//                    Log.i("msg", "I am here3");
+//                    if (resultCode == RESULT_OK && data != null) {
+//                        Log.i("msg", "I am here4");
+//                        Uri selectedImage = data.getData();
+//                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+//                        if (selectedImage != null) {
+//                            Log.i("msg", "I am here5");
+//                            Cursor cursor = getContentResolver().query(selectedImage,
+//                                    filePathColumn, null, null, null);
+//                            if (cursor != null) {
+//                                Log.i("msg", "I am here6");
+//                                cursor.moveToFirst();
+//                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//                                String picturePath = cursor.getString(columnIndex);
+//                                imageFile = new File(getApplicationContext().getFilesDir(),picturePath.substring(picturePath.lastIndexOf("/")+1));
+//
+//
+//
+////                                try (InputStream stream = getApplicationContext().getContentResolver().openInputStream(selectedImage)) {
+////                                    OutputStream outputStream = new FileOutputStream(imageFile);
+//////                                    IOUtils.copy(stream,outputStream);
+////                                }
+////                                catch(IOException e){
+////                                    e.printStackTrace();
+////                                }
+//                                img.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//                                cursor.close();
+//                                Log.i("msg", "I am here7");
+//                            }
+//                        }
+//                        Log.i("msg", "I am here8");
+//                        img.setImageURI(selectedImage);
+//                    }
+//                    break;
+//            }
+//        }
+    }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            img.setImageBitmap(imageBitmap);
+//        }
+//    }
+
+//    String currentPhotoPath;
+//
+//    private File createImageFile() throws IOException {
+//        // Create an image file name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        );
+//
+//        // Save a file: path for use with ACTION_VIEW intents
+//        currentPhotoPath = image.getAbsolutePath();
+//        return image;
+//    }
+
+
 }
